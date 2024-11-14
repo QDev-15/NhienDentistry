0aa5c67d5047a3836f443f21cf0592ffa4a6ecb1
Nguyễn Hữu Quỳnh <quynhvpit@outlook.com>
2024-11-14 09:41:35 +0700

    update navbar

---
+++ NhienDentistry.Web/wwwroot/js/navbar-mega.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    // executes when HTML-Document is loaded and DOM is ready
+
+    // breakpoint and up  
+    $(".navbar-mega .dropdown-toggle").hover(function () {
+        $(this).parent().toggleClass("show");
+        $(this).parent().find(".dropdown-menu").toggleClass("show");
+    });
+
+    // hide the menu when the mouse leaves the dropdown
+    $(".navbar-mega .dropdown-menu").mouseleave(function () {
+        $(this).removeClass("show");
+    });
+
+
+
+    // document ready  
+});
